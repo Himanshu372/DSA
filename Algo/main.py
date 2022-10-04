269b2026b7b0be5d2cbcb0b322f1995973fd97b3
Himanshu372 <jagtaphimanshu09@gmail.com>
2022-10-04 23:02:16 +0530

    Started with sorting problems

--- Algo/main.py
+++ Algo/main.py
@@ -1,7 +1,20 @@
 from Search.search import Search
+from Sort.sort import Sort
+
 
 if __name__ == "__main__":
     print("Started Execution")
+    print("'Search' problems started")
     s = Search([0, 20, 30, 50, 60, 80, 110, 130, 140, 170])
     by_binary = s.binary_search(110)
+    by_ternary = s.ternary_serach(110)
     print(f"Index by binary search: {by_binary}")
+    print(f"Index by ternary search: {by_ternary}")
+    print("'Search' problems end")
+
+    print("'Sorting' problems started")
+    sr = Sort([0, 30, 4, 78, 33, 56, 59, -110, 13, -1, 170])
+    by_bubble_sort = sr.bubble_sort()
+    print(f"Array after sorting by bubble sort {by_bubble_sort}")
+
+
